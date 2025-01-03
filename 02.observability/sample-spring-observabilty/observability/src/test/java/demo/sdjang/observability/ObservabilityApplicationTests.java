b82d41ea528c4155e3d7472fb4f64b326f6f1de0
sdjang6253 <qkswl625341@gmail.com>
2025-01-03 22:12:21 +0900

    text image initial commit

---
+++ 02.observability/sample-spring-observabilty/observability/src/test/java/demo/sdjang/observability/ObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.sdjang.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
