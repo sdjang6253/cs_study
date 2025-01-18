d0babef4f6fea67d2a1c79f2337e915e60b33bb9
sdjang6253 <qkswl625341@gmail.com>
2025-01-18 13:33:15 +0900

    (색션9) 빈 생명주기 콜백 - initMethod , destroyMethod PreDestory , PostConstruct 이는 javax.* 로 시작하는 자바 진영의 표준이다. 가능하면 이를 사용하자 ( 스프링에서도 추천 ) , 하지만 외부 라이브러리에서는 사용하지 못하기에 그때는 destroyMethod , initMethod 사용하면 된다.

--- 03.spring_basic/hello/src/test/java/learn/basic/hello/lifecycle/NetworkClient.java
+++ 03.spring_basic/hello/src/test/java/learn/basic/hello/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package learn.basic.hello.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient  {
 
     private String url ;
@@ -31,14 +34,14 @@ public class NetworkClient  {
         System.out.println("close : " + url + " , hostname = " + hostname);
     }
 
-
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-
+    @PreDestroy
     public void close()  {
         System.out.println("NetworkClient.close");
         disconnect();
