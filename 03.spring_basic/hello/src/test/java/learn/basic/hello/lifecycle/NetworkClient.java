ebfa484443af48d2b1b53289beccd2b9f2644108
sdjang6253 <qkswl625341@gmail.com>
2025-01-18 13:25:22 +0900

    (색션9) 빈 생명주기 콜백 - 인터페이스 - InitializingBean , DisposableBean 단점은 외부 라이브러리 등에 적용하지 못하고, 함수명등이 제약이 있음. hostname 은 임의로 추가해 봄

--- 03.spring_basic/hello/src/test/java/learn/basic/hello/lifecycle/NetworkClient.java
+++ 03.spring_basic/hello/src/test/java/learn/basic/hello/lifecycle/NetworkClient.java
@@ -1,32 +1,49 @@
 package learn.basic.hello.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient  {
+public class NetworkClient  implements InitializingBean , DisposableBean {
 
     private String url ;
+    private String hostname ;
 
     public NetworkClient() {
-        System.out.println("생성자 호출 , url = " + url );
+        System.out.println("생성자 호출 , url = " + url + " , hostname = " + hostname );
+    }
+
+    public void setUrl(String url) {
+        System.out.println("NetworkClient.setUrl");
+        this.url = url;
+    }
+
+    public void setHostname(String hostname) {
+        System.out.println("NetworkClient.setHostname");
+        this.hostname = hostname;
+    }
+
+    public void connect() {
+        System.out.println("connect : "  + url + " , hostname = " + hostname);
+    }
+
+    public void call (String message) {
+        System.out.println("call = " + url + " message = " + message + " , hostname = " + hostname);
+    }
+
+    public void disconnect() {
+        System.out.println("close : " + url + " , hostname = " + hostname);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
-
-    public void connect() {
-        System.out.println("connect : "  + url);
-    }
-
-    public void call (String message) {
-        System.out.println("call = " + url + " message = " + message);
-    }
-
-    public void disconnect() {
-        System.out.println("close : " + url);
-    }
-
-
 }
