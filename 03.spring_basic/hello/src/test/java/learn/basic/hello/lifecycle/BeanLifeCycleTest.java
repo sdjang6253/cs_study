0e054cd88552362ee4ecf973e60ea490acaa206f
sdjang6253 <qkswl625341@gmail.com>
2025-01-18 13:17:29 +0900

    (색션9) 빈 생명주기 콜백 - 빈 생명주기 콜백 시작 -> 생성과 초기화를 나누는 방식이 유지보수 관점에서 좀더 좋을 수 있다.

---
+++ 03.spring_basic/hello/src/test/java/learn/basic/hello/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package learn.basic.hello.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = context.getBean(NetworkClient.class);
+        context.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
