2f27d259f807f367b65c93d57ee7ad823fe67121
sdjang6253 <qkswl625341@gmail.com>
2025-06-14 11:25:30 +0900

    색션2. Object 클래스 - 6.Object클래스

---
+++ 04.java_middle1/java_middle_1/src/lang/Object/ObjectMain.java
@@ -0,0 +1,13 @@
+package lang.Object;
+
+public class ObjectMain {
+    public static void main(String[] args) {
+
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        String str = child.toString();
+        System.out.println(str);
+    }
+}
---
+++ 04.java_middle1/java_middle_1/src/lang/Object/Parent.java
@@ -0,0 +1,8 @@
+package lang.Object;
+
+public class Parent extends Object{
+    public void parentMethod() {
+
+        System.out.println("parent.parentMethod");
+    }
+}
